9987edfcdd9338d905858c498f94da6e89d83ea4
Ji, Soonki <soonki.ji@etrade.com>
2021-07-16 09:13:06 -0400

    Corrected local path.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-set PATH=C:\Users\Soonki\AppData\Local\Programs\Python\Python38-32;%PATH%
+set PATH=%LOCALAPPDATA%\Programs\Python\Python38-32;%PATH%
 if not exist venv python -m venv venv
 call venv\scripts\activate
 python -m pip install --upgrade pip
